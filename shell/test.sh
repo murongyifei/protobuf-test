f879ab7266df4ac95b2f98a7d5ddaffa506003b6
wanghaiming <252127886@qq.com>
2022-02-05 21:58:08 +0800

    完善shell

--- shell/test.sh
+++ shell/test.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+cd `dirname $0`
+BIN_DIR=`pwd`
+cd ..
+DEPLOY_DIR=`pwd`
+
+echo $BIN_DIR
+echo $DEPLOY_DIR
+
 #echo "whm hello world!!"
-ppath=/Volumes/one/IdeaProjects/pbTestMaven/src/main/proto
+ppath=$DEPLOY_DIR/src/main/proto
+echo $ppath
 jpath=/Users/whm/one/proto/java
 opath=/Users/whm/one/proto/objc
 echo "start..."
 /usr/local/bin/protoc --proto_path=$ppath --java_out=$jpath --objc_out=$opath $ppath/Common.proto
 /usr/local/bin/protoc --proto_path=$ppath --java_out=$jpath --objc_out=$opath $ppath/Student.proto
+echo "end..."
+echo "open $jpath"
+open $jpath
